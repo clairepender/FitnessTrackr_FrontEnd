a6bdb9f982ce5c526b9a3bf714d1de0435ace687
clairepender <clairepender5@gmail.com>
2021-11-26 12:36:48 -0600

    working on login/logout redirects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const NavBar = ({token, setToken}) => {
+    const history = useHistory();
+    function loginRedirect() {
+        history.push('/login');
+    }
 
     return (
         <nav>
                 <div id="nav-bar">
                     <Link to="/">Home </Link>
-                    <Link to="/login">Login </Link>
                     <Link to="/register">Sign Up </Link>
                     <Link to="/myroutines">My Routines </Link>
-                    <Link to="/routines">Public Routines</Link>
+                    <Link to="/routines">Public Routines </Link>
+                    <Link to="/login">Login</Link>
+                    {/* {(!token ? <span className="nav-link active" to="/login" onClick={() => {
+                        loginRedirect();
+                    }}>Log In</span> : 
+                            <Link className="nav-link active" to="/logout" onClick={(event) => {
+                                localStorage.removeItem("token");
+                                setToken("");
+                        }}>Log Out</Link>)} */}
                 </div>
         </nav>
     )
