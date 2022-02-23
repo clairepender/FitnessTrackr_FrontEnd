dd3c3fca5b5bfc8c78c690716d1c4f84c6ac9448
clairepender <clairepender5@gmail.com>
2022-02-23 10:50:52 -0600

    added border

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,10 @@ const Home = () => {
             <div className="text-center font-spartan">
                 <h2>Let's do this.</h2>
             </div>
-            <img src="https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80"
+            <div className="homeImage"><img src="https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80"
             className="img-card"
             style={{width: 600, height: 400}} />
+            </div>
            
         </div>
     )
